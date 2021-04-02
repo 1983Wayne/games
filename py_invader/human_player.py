a66d521e09d80b01d58b20fefa52aea74a537f98
Wayne <61163734+1983Wayne@users.noreply.github.com>
2021-04-02 08:43:25 -0600

    Update human_player.py

--- py_invader/human_player.py
+++ py_invader/human_player.py
@@ -1,4 +1,4 @@
-# This is the player themself and will take care of the simple things for the playable spaceship.
+# This is the player class.  Will take care of the simple things for the player.
 
 class Player:
     """The player will be able to move left and right, fire bullets or missiles (Eventually.)
@@ -7,7 +7,9 @@ class Player:
         self.x = x_pos
         self.y = y_pos
         self.lives = 3
-        self.bullets = [] # Added to list when method called
+        self.bullet_timer_MAX = 5
+        self.bullet_timer = 5                                                  # Cooldown for shooting
+        self.bullets = []                                                       # Added to list when method called
         
     def fire_bullet(self):
         self.bullets.append([self.x, self.y])
@@ -17,3 +19,10 @@ class Player:
         
     def move_player(self, player_speed):
         self.x += player_speed
+    
+    def change_bullet_timer(self):
+        if len(self.bullets) == 0 or self.bullet_timer == 0:
+            self.bullet_timer = self.bullet_timer_MAX
+            
+        else:
+            self.bullet_timer -= 1
